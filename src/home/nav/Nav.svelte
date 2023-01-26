d266fa6eb0ae2abf38bccfdad347d22565e1822c
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-26 16:01:05 +0100

    feat : define navbar

--- src/home/nav/Nav.svelte
+++ src/home/nav/Nav.svelte
@@ -1,24 +1,54 @@
 <script lang="ts">
     import './nav.scss';
+    import {FontAwesomeIcon} from '@fortawesome/svelte-fontawesome';
+    import {faBars, faQuestion} from '@fortawesome/free-solid-svg-icons'
+    import {faInstagram, faYoutube, faFlickr} from '@fortawesome/free-brands-svg-icons'
+
 </script>
 
 <nav>
     <div class="halo"></div>
     <div class="top-white-banner">
-        <div class="container">
+        <div class="banner-container">
             <div class="logos">
-                <div class="logo-jmj"></div>
-                <div class="logo-C4Y"></div>
+                <div class="logo-jmj">
+                    <img src="/img/JMJ.svg" alt="JMJ Libsoa 2023 logo">
+                </div>
+                <div class="logo-C4Y">
+                    <img src="/img/C4Y.svg" alt="Church4You.be Logo">
+                </div>
             </div>
         </div>
     </div>
-    <div class="buttons">
-        <div class="green"></div>
-        <div class="yellow"></div>
+    <div class="bottom-right-banner"></div>
+    <div class="bottom-left-banner"></div>
+
+    <div class="menus">
+        <div class="green"><FontAwesomeIcon icon={faBars} /></div>
+        <div class="yellow"><FontAwesomeIcon icon={faQuestion} /></div>
     </div> 
-    <div class="text-2xl uppercase text-center font-bold">
-        <p class="ml-[-15vw]">en route</p>
-        <p class="ml-[15vw]">pour les JMJ</p>
+    <div class="main-title">
+        <p>en route</p>
+        <p>pour les JMJ</p>
         <p>avec la belgique</p>
+    </div>
+    <div class="secondary-title">
+        <div class="text-green">Les JMJ c'est quoi ?</div>
+        <div class="text-red">Différentes propositions</div>
+        <div class="text-orange">Infos Church4You</div>
+    </div>
+    <div class="socials">
+        <div class="social">
+            <FontAwesomeIcon icon={faInstagram} />
+        </div>
+        <div class="social">
+            <FontAwesomeIcon icon={faYoutube} />
+        </div>
+        <div class="social">
+            <FontAwesomeIcon icon={faFlickr} />
+        </div>
+    </div>
+    <div class="nav-footer">
+        <div class="top-border"></div>
     </div>
 </nav>
