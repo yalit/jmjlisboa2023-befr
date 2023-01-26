d67d580e151cedd8fc86a4b0602fc300bf45cfe5
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-26 01:22:28 +0100

    feat : add first base for nav

--- src/home/nav/Nav.svelte
+++ src/home/nav/Nav.svelte
@@ -3,5 +3,22 @@
 </script>
 
 <nav>
-    
+    <div class="halo"></div>
+    <div class="top-white-banner">
+        <div class="container">
+            <div class="logos">
+                <div class="logo-jmj"></div>
+                <div class="logo-C4Y"></div>
+            </div>
+        </div>
+    </div>
+    <div class="buttons">
+        <div class="green"></div>
+        <div class="yellow"></div>
+    </div> 
+    <div class="text-2xl uppercase text-center font-bold">
+        <p class="ml-[-15vw]">en route</p>
+        <p class="ml-[15vw]">pour les JMJ</p>
+        <p>avec la belgique</p>
+    </div>
 </nav>
