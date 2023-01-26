19c458542bcb05e5e1e986d38dde9b3539aad052
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-26 23:37:53 +0100

    feat : add hero green

--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Nav from "./nav/Nav.svelte";
-  import Hero1 from "./hero1/hero1.svelte";
+  import HeroGreen from "./heroGreen/heroGreen.svelte";
+  import HeroRoutes from "./heroRoutes/heroRoutes.svelte";
 </script>
 
 <Nav />
-<Hero1 />
+<HeroGreen />
+<HeroRoutes />
---
+++ src/home/heroRoutes/heroRoutes.svelte
@@ -0,0 +1,9 @@
+<section></section>
+
+<style>
+    section {
+        height: 50vh;
+        width: 100vw;
+        background-color: white;
+    }
+</style>
