eebb1c2654f86ef3cc7d1faaff9135d6ea25b74d
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-27 12:00:45 +0100

    feat : addition of the footer to fix the bottom of the page

--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import Nav from "./nav/Nav.svelte";
   import HeroGreen from "./heroGreen/heroGreen.svelte";
-  import HeroRoutes from "./heroRoutes/heroRoutes.svelte";
+  import Footer from "./footer/footer.svelte";
 </script>
 
 <Nav />
 <HeroGreen />
-<HeroRoutes />
+<Footer />
---
+++ src/home/footer/footer.svelte
@@ -0,0 +1,13 @@
+<section>
+    FOOTER
+</section>
+
+<style>
+    section {
+        width: 100vw;
+        height: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
