f54d8c72039c1da5c4a92acc4bcd9f5436a6d572
Nann1ck <3314374+yalit@users.noreply.github.com>
2023-01-27 18:03:50 +0100

    feat : start of heroRoute (#22)
    Co-authored-by: Yannick Alsberge <y.alsberge@yalit.be>

--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Nav from "./nav/Nav.svelte";
   import HeroGreen from "./heroGreen/heroGreen.svelte";
+  import HeroRoutes from "./heroRoutes/heroRoutes.svelte";
   import Footer from "./footer/footer.svelte";
 </script>
 
 <Nav />
 <HeroGreen />
+<HeroRoutes />
 <Footer />
--- src/home/heroRoutes/heroRoutes.svelte
+++ src/home/heroRoutes/heroRoutes.svelte
@@ -1,9 +1,89 @@
-<section></section>
+<script lang="ts">
+    import './heroRoutes.scss';
+    import Carousel from 'svelte-carousel';
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+    import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-<style>
-    section {
-        height: 50vh;
-        width: 100vw;
-        background-color: white;
+    const images = [
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'https://church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'https://church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'https://church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'https://church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'https://church4you.be'
+        }
+    ]
+
+    let nbImagesToDisplay = getNbImageToDisplay();
+    const handleResize = () => {
+            nbImagesToDisplay = getNbImageToDisplay();
     }
-</style>
+
+    function getNbImageToDisplay() {
+        if (window.innerWidth < 640) {
+            return 3;
+        } else if (window.innerWidth < 960) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+</script>
+
+<svelte:window on:resize={handleResize}/>
+
+<section id="heroRoutes">
+    <div class="section-container">
+
+        <div class="allroads">
+            En 2023, tous les chemins mènent à Lisbonne
+        </div>
+
+        <div class="carousel">
+            <Carousel
+                particlesToShow={nbImagesToDisplay}
+                particlesToScroll={1}
+                dots={false}
+                swiping={true}
+                let:showPrevPage
+                let:showNextPage
+            >
+                {#each images as image}
+                    <a href="{image.link}" target="_blank" rel="noreferrer">
+                        <img src="{image.url}" alt="{image.alt}" />
+                    </a>  
+                {/each}
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div slot="prev" on:click={showPrevPage} class="dot prev">
+                    <FontAwesomeIcon icon={faAngleLeft}/>
+                </div>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div slot="next" on:click={showNextPage} class="dot next">
+                    <FontAwesomeIcon icon={faAngleRight}/>
+                  </div>
+            </Carousel>
+        </div>
+
+        <div class="action">
+            <p><a href="#">Voir les différentes routes belges</a></p>
+        </div>
+    </div>
+</section>
