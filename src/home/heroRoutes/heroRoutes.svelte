b2f14b5d09cbdb6264f0a57224396c82699f8d62
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-27 18:01:56 +0100

    feat : add carousel

--- src/home/heroRoutes/heroRoutes.svelte
+++ src/home/heroRoutes/heroRoutes.svelte
@@ -1,36 +1,54 @@
 <script lang="ts">
     import './heroRoutes.scss';
     import Carousel from 'svelte-carousel';
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+    import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
     const images = [
         {
             url: '/img/heroRoutes/routeX.svg',
             alt: "Logo de la route X",
-            link: 'church4you.be'
+            link: 'https://church4you.be'
         },
         {
             url: '/img/heroRoutes/routeX.svg',
             alt: "Logo de la route X",
-            link: 'church4you.be'
+            link: 'https://church4you.be'
         },
         {
             url: '/img/heroRoutes/routeX.svg',
             alt: "Logo de la route X",
-            link: 'church4you.be'
+            link: 'https://church4you.be'
         },
         {
             url: '/img/heroRoutes/routeX.svg',
             alt: "Logo de la route X",
-            link: 'church4you.be'
+            link: 'https://church4you.be'
         },
         {
             url: '/img/heroRoutes/routeX.svg',
             alt: "Logo de la route X",
-            link: 'church4you.be'
+            link: 'https://church4you.be'
         }
     ]
 
+    let nbImagesToDisplay = getNbImageToDisplay();
+    const handleResize = () => {
+            nbImagesToDisplay = getNbImageToDisplay();
+    }
+
+    function getNbImageToDisplay() {
+        if (window.innerWidth < 640) {
+            return 3;
+        } else if (window.innerWidth < 960) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
 </script>
+
+<svelte:window on:resize={handleResize}/>
 
 <section id="heroRoutes">
     <div class="section-container">
@@ -41,21 +59,31 @@
 
         <div class="carousel">
             <Carousel
-                particlesToShow={5}
+                particlesToShow={nbImagesToDisplay}
                 particlesToScroll={1}
                 dots={false}
-                arrows={false}
+                swiping={true}
+                let:showPrevPage
+                let:showNextPage
             >
                 {#each images as image}
-                    <a href="{image.link}">
+                    <a href="{image.link}" target="_blank" rel="noreferrer">
                         <img src="{image.url}" alt="{image.alt}" />
                     </a>  
                 {/each}
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div slot="prev" on:click={showPrevPage} class="dot prev">
+                    <FontAwesomeIcon icon={faAngleLeft}/>
+                </div>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div slot="next" on:click={showNextPage} class="dot next">
+                    <FontAwesomeIcon icon={faAngleRight}/>
+                  </div>
             </Carousel>
         </div>
 
         <div class="action">
-            <p>Voir les différentes routes belges</p>
+            <p><a href="#">Voir les différentes routes belges</a></p>
         </div>
     </div>
 </section>
