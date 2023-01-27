eb130072a9f70bf17fc1c8aaff3b555e368cdcf0
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-27 13:48:43 +0100

    feat : start of heroRoute

--- src/home/heroRoutes/heroRoutes.svelte
+++ src/home/heroRoutes/heroRoutes.svelte
@@ -1,9 +1,61 @@
-<section></section>
+<script lang="ts">
+    import './heroRoutes.scss';
+    import Carousel from 'svelte-carousel';
 
-<style>
-    section {
-        height: 50vh;
-        width: 100vw;
-        background-color: white;
-    }
-</style>
+    const images = [
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'church4you.be'
+        },
+        {
+            url: '/img/heroRoutes/routeX.svg',
+            alt: "Logo de la route X",
+            link: 'church4you.be'
+        }
+    ]
+
+</script>
+
+<section id="heroRoutes">
+    <div class="section-container">
+
+        <div class="allroads">
+            En 2023, tous les chemins mènent à Lisbonne
+        </div>
+
+        <div class="carousel">
+            <Carousel
+                particlesToShow={5}
+                particlesToScroll={1}
+                dots={false}
+                arrows={false}
+            >
+                {#each images as image}
+                    <a href="{image.link}">
+                        <img src="{image.url}" alt="{image.alt}" />
+                    </a>  
+                {/each}
+            </Carousel>
+        </div>
+
+        <div class="action">
+            <p>Voir les différentes routes belges</p>
+        </div>
+    </div>
+</section>
