92dff3e200ac64c019b31cd857abd588d5bc7d2b
Yannick Alsberge <y.alsberge@yalit.be>
2023-01-28 12:08:38 +0100

    feat : init heroRed section

--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -2,6 +2,7 @@
   import Nav from "./nav/Nav.svelte";
   import HeroGreen from "./heroGreen/heroGreen.svelte";
   import HeroRoutes from "./heroRoutes/heroRoutes.svelte";
+  import HeroRed from "./heroRed/heroRed.svelte";
   import HeroYellow from "./heroYellow/heroYellow.svelte";
   import Footer from "./footer/footer.svelte";
 </script>
@@ -9,5 +10,6 @@
 <Nav />
 <HeroGreen />
 <HeroRoutes />
+<HeroRed />
 <HeroYellow />
 <Footer />
---
+++ src/home/heroRed/heroRed.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import './heroRed.scss'
+</script>
+
+<section id="heroRed">
+    <div class="section-container">
+        <div class="animated-questions">
+
+        </div>
+
+        <div class="1000questions">
+            <p>Tu te poses <span class="emphasized">1000 questions</span> sur ce périple ?</p>
+        </div>
+
+        <div class="actions">
+            <div class="action"><a href="#">Consulte notre FAQ</a></div>
+            <div class="action"><a href="#">On verra cela après, je me lance</a></div>
+            <div class="action"><a href="#">S'inscrire à la route Church4You</a></div>
+        </div>
+    </div>
+</section>
