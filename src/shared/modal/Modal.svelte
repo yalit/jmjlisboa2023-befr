a38929914a4bc622f33ff228a09cc0030c6e9ba0
Yannick Alsberge <y.alsberge@yalit.be>
2023-02-10 23:01:44 +0100

    feat : setup modal for pope message

--- src/shared/modal/Modal.svelte
+++ src/shared/modal/Modal.svelte
@@ -20,9 +20,9 @@
 <svelte:window on:keyup={on_key_up}/>
 
 <div class="relative z-10 {hidden}" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" on:click={close}></div>
+    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
     <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="flex min-h-full items-center justify-center p-0 text-center sm:items-center sm:p-0">
+        <div class="flex min-h-full items-center justify-center p-0 text-center sm:items-center sm:p-0" on:click={close}>
             <div class="modal relative w-10/12 transform overflow-hidden rounded-lg bg-white p-0 text-left shadow-xl transition-all sm:my-8 md:w-8/12">
                 <div class="modal-close" on:click={close}><FontAwesomeIcon icon={faTimes} /></div>
                 <slot name="content"></slot>
