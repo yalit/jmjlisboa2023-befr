05a5559df6bbc8838dd4b157e12c8cddb921b785
Yannick Alsberge <y.alsberge@yalit.be>
2023-02-20 18:23:48 +0100

    feat : setup page c4Y

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import postcss from './postcss.config.cjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        quoi: resolve(__dirname, 'cestquoi.html'),
+        proposal: resolve(__dirname, 'proposal.html'),
+        c4y: resolve(__dirname, 'c4y.html'),
+        faq: resolve(__dirname, 'faq.html'),
+      },
+    },
+  },
   base: './',
   plugins: [svelte()],
   css:{
