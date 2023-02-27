dfe989901b3a02adf2e28e1eedbf6011edd233da
Yannick <3314374+yalit@users.noreply.github.com>
2023-02-27 21:09:53 +0100

    feat : add footer (#50)

--- src/shared/footer/footer.svelte
+++ src/shared/footer/footer.svelte
@@ -1,13 +1,27 @@
-<section>
-    FOOTER
-</section>
+<script lang="ts">
+    import './footer.scss';
+</script>
 
-<style>
-    section {
-        width: 100%;
-        height: 100vw;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-</style>
+<section>
+    <div class="logos_c4y">
+        <img src="/img/navbar/C4Y.svg" alt="Logo de Church4You.be">
+        <img src="/img/logos/C4Y_BW.png" alt="Logo de la Pastorale des Jeunes du Brabant-Wallong">
+        <img src="/img/logos/C4Y_BXL.jpg" alt="Logo de la Pastorale des Jeunes de Bruxelles">
+        <img src="/img/logos/C4Y_Liege.jpg" alt="Logo de la Pastorale des Jeunes de Liège">
+        <img src="/img/logos/C4Y_Namur.png" alt="Logo de la Pastorale des Jeunes de Namur">
+        <img src="/img/logos/C4Y_Tournai.png" alt="Logo de la Pastorale des Jeunes de Tournai">
+    </div>
+    <div class="footer_text">
+        Nous travaillons ensemble sur la route C4Y
+    </div>
+    <div class="logos_partenaires">
+        <div><img src="/img/logos/emmanuel.png" alt="Logo de la Communauté de l'Emmanuel"></div>
+        <div><img src="/img/logos/fondation.svg" alt="Logo de Fondacio"></div>
+        <div><img src="/img/logos/chemin_neuf.png" alt="Logo de la Communauté du Chemin Neuf"></div>
+        <div><img src="/img/logos/marie%20jeunesse.png" alt="Logo de la Famille Marie-Jeunesse"></div>
+        <div><img src="/img/logos/assomption.png" alt="Logo des Augustins de l'Assomption"></div>
+        <div><img src="/img/logos/religieuses_assomption.png" alt="Logo des Religieuses de l'Assomption"></div>
+        <div><img src="/img/logos/tiberiade.png" alt="Logo de la fraternité de Tibériade"></div>
+        <div><img src="/img/logos/serviteur.svg" alt="Logo des Serviteurs de l'Amour"></div>
+    </div>
+</section>
