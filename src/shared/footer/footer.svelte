d133821567dbe8f35ba32911cca2ada93b322f89
Yannick <3314374+yalit@users.noreply.github.com>
2023-03-20 12:26:21 +0100

    feat : update links in footer (#58) (#61)
    Co-authored-by: Yannick Alsberge <y.alsberge@yalit.be>

--- src/shared/footer/footer.svelte
+++ src/shared/footer/footer.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
+    import links, { CHURCH4YOU } from '../../lib/data/links';
     import './footer.scss';
+
 </script>
 
 <section>
     <div class="logos_c4y">
-        <img src="/img/navbar/C4Y.svg" alt="Logo de Church4You.be">
-        <img src="/img/logos/C4Y_BW.png" alt="Logo de la Pastorale des Jeunes du Brabant-Wallong">
-        <img src="/img/logos/C4Y_BXL.jpg" alt="Logo de la Pastorale des Jeunes de Bruxelles">
-        <img src="/img/logos/C4Y_Liege.jpg" alt="Logo de la Pastorale des Jeunes de Liège">
-        <img src="/img/logos/C4Y_Namur.png" alt="Logo de la Pastorale des Jeunes de Namur">
-        <img src="/img/logos/C4Y_Tournai.png" alt="Logo de la Pastorale des Jeunes de Tournai">
+        <a href="{links[CHURCH4YOU]}" target="_blank" rel="noreferrer"><img src="/img/navbar/C4Y.svg" alt="Logo de Church4You.be"></a>
+        <a href="https://church4you.be/brabant-wallon/" target="_blank" rel="noreferrer"><img src="/img/logos/C4Y_BW.png" alt="Logo de la Pastorale des Jeunes du Brabant-Wallong"></a>
+        <a href="https://church4you.be/bruxelles/" target="_blank" rel="noreferrer"><img src="/img/logos/C4Y_BXL.jpg" alt="Logo de la Pastorale des Jeunes de Bruxelles"></a>
+        <a href="https://church4you.be/liege/" target="_blank" rel="noreferrer"><img src="/img/logos/C4Y_Liege.jpg" alt="Logo de la Pastorale des Jeunes de Liège"></a>
+        <a href="https://church4you.be/namur-lux/" target="_blank" rel="noreferrer"><img src="/img/logos/C4Y_Namur.png" alt="Logo de la Pastorale des Jeunes de Namur"></a>
+        <a href="https://church4you.be/tournai/" target="_blank" rel="noreferrer"><img src="/img/logos/C4Y_Tournai.png" alt="Logo de la Pastorale des Jeunes de Tournai"></a>
     </div>
     <div class="footer_text">
         Nous travaillons ensemble sur la route C4Y
     </div>
     <div class="logos_partenaires">
-        <div><img src="/img/logos/emmanuel.png" alt="Logo de la Communauté de l'Emmanuel"></div>
-        <div><img src="/img/logos/fondation.svg" alt="Logo de Fondacio"></div>
-        <div><img src="/img/logos/chemin_neuf.png" alt="Logo de la Communauté du Chemin Neuf"></div>
-        <div><img src="/img/logos/marie%20jeunesse.png" alt="Logo de la Famille Marie-Jeunesse"></div>
-        <div><img src="/img/logos/assomption.png" alt="Logo des Augustins de l'Assomption"></div>
-        <div><img src="/img/logos/religieuses_assomption.png" alt="Logo des Religieuses de l'Assomption"></div>
-        <div><img src="/img/logos/tiberiade.png" alt="Logo de la fraternité de Tibériade"></div>
-        <div><img src="/img/logos/serviteur.svg" alt="Logo des Serviteurs de l'Amour"></div>
+        <div><a href="https://emmanuelco.be/fr/" target="_blank" rel="noreferrer"><img src="/img/logos/emmanuel.png" alt="Logo de la Communauté de l'Emmanuel"></a></div>
+        <div><a href="https://fondacio.be/" target="_blank" rel="noreferrer"><img src="/img/logos/fondation.svg" alt="Logo de Fondacio"></a></div>
+        <div><a href="https://www.chemin-neuf.be/fr/" target="_blank" rel="noreferrer"><img src="/img/logos/chemin_neuf.png" alt="Logo de la Communauté du Chemin Neuf"></a></div>
+        <div><a href="https://www.marie-jeunesse.org/" target="_blank" rel="noreferrer"><img src="/img/logos/marie%20jeunesse.png" alt="Logo de la Famille Marie-Jeunesse"></a></div>
+        <div><a href="https://www.assomption.org/qui-sommes-nous/" target="_blank" rel="noreferrer"><img src="/img/logos/assomption.png" alt="Logo des Augustins de l'Assomption"></a></div>
+        <div><a href="https://assumpta.org/fr/" target="_blank" rel="noreferrer"><img src="/img/logos/religieuses_assomption.png" alt="Logo des Religieuses de l'Assomption"></a></div>
+        <div><a href="https://www.tiberiade.be/" target="_blank" rel="noreferrer"><img src="/img/logos/tiberiade.png" alt="Logo de la fraternité de Tibériade"></a></div>
+        <div><a href="https://www.serviteur.be/" target="_blank" rel="noreferrer"><img src="/img/logos/serviteur.svg" alt="Logo des Serviteurs de l'Amour"></a></div>
     </div>
 </section>
