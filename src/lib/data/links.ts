dc07a2611fde615ebdc4e7f58d4a3ffe850d4d4c
Yannick Alsberge <y.alsberge@yalit.be>
2023-03-20 12:49:40 +0100

    feat : update menu and links (#57)

--- src/lib/data/links.ts
+++ src/lib/data/links.ts
@@ -1,9 +1,22 @@
 export const CHURCH4YOU = "c4y";
+export const LISBOA2023 = 'lisboa2023';
 export const REGISTER = 'register';
 
+export const C4Y_PAGE = 'c4y_page'
+export const CESTQUOI_PAGE = 'cestquoi_page';
+export const BLOG_PAGE = 'blog_page';
+export const PROPOSALS_PAGE = 'proposals_page';
+export const FAQ_PAGE = 'faq_page';
+
 const links: {[k: string]: string} = {
-  "c4y": 'https://church4you.be',
-  "register": 'https://registration-wyd2023.chemin-neuf.org/fr/home-belgique/'
+  [CHURCH4YOU]: 'https://church4you.be',
+  [LISBOA2023]: 'https://www.lisboa2023.org/fr',
+  [REGISTER]: 'https://registration-wyd2023.chemin-neuf.org/fr/home-belgique/',
+  [C4Y_PAGE] : 'c4y.html',
+  [CESTQUOI_PAGE] : 'cestquoi.html',
+  [BLOG_PAGE] : 'blog.html',
+  [PROPOSALS_PAGE] : 'proposal.html',
+  [FAQ_PAGE] : 'faq.html'
 }
 
 export default links;
